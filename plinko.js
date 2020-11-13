8cd8283fd173f8735ff6f1ec5147eb644014f70a
AdhirajRanjan <71758327+AdhirajRanjan@users.noreply.github.com>
2020-11-13 21:02:41 +0530

    Add files via upload

---
+++ plinko.js
@@ -0,0 +1,28 @@
+
+
+  class Plinko {
+    constructor(x,y,r) 
+    {
+      var options = 
+      {
+        isStatic: true
+      }
+      this.r=r
+
+      this.body = Bodies.circle(x,y, this.r,options);
+      World.add(world, this.body);
+    }
+    display(){
+      var pos =this.body.position;
+      var angle = this.body.angle;
+
+      push()
+      translate(pos.x, pos.y);
+      rotate(angle);
+      noStroke();
+      fill("white");
+      ellipseMode(RADIUS);
+      ellipse(pos.x, pos.y, this.r);
+      pop()
+    }
+  };
